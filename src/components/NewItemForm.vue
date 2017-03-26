687a532ffeb99c0a1dd1c64ed8035f7f268c77bf
Chris Perko <cperko@teachers-teachers.com>
2017-03-26 15:57:55 -0500

    Adding items to the list

---
+++ src/components/NewItemForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrapper">
+    <input type="text" v-model="newName" />
+    <button v-on:click="create(newName)">Create</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'new-item-form',
+  props: ['create', 'newName']
+}
+</script>
+
+<style scoped>
+</style>
--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -2,6 +2,11 @@
   <div>
     <h1>Todo List</h1>
 
+    <new-item-form 
+      :newName="newName" 
+      :create="onCreate">
+    </new-item-form>
+
     <todo-item 
       v-for="todo in todos" 
       :item="todo">
@@ -12,18 +17,33 @@
 
 <script>
 import TodoItem from '@/components/TodoItem'
+import NewItemForm from '@/components/NewItemForm'
 
 export default {
   name: 'todo-app',
   components: {
-    'todo-item': TodoItem
+    'todo-item': TodoItem,
+    'new-item-form': NewItemForm
   },
   data () {
     return {
       todos: [
         { id: 1, name: 'Study Vue' },
         { id: 2, name: 'Make something awesome!' }
-      ]
+      ],
+      index: 3,
+      newName: ''
+    }
+  },
+  methods: {
+    onCreate: function (name) {
+      if (!name) return
+
+      this.todos.push({
+        id: this.index++, name
+      })
+
+      this.newName = ''
     }
   }
 }
