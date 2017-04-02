86b9cca57bd40c183482fdb67e2dc05e6f298d77
Chris Perko <cperko@teachers-teachers.com>
2017-04-02 15:56:43 -0500

    Can now complete items

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -4,10 +4,13 @@
 
     <new-item-form :create="onCreate"></new-item-form>
 
-    <todo-item 
-      v-for="todo in todos" 
-      :item="todo">
-    </todo-item>
+    <div class="items">
+      <todo-item 
+        v-for="todo in itemsToDo" 
+        :item="todo"
+        :onDone="onDone">
+      </todo-item>
+    </div>
   </div>
 </template>
 
@@ -25,8 +28,8 @@ export default {
   data () {
     return {
       todos: [
-        { id: 1, name: 'Study Vue' },
-        { id: 2, name: 'Make something awesome!' }
+        { id: 1, name: 'Study Vue', isDone: false },
+        { id: 2, name: 'Make something awesome!', isDone: false }
       ],
       index: 3
     }
@@ -36,7 +39,23 @@ export default {
       if (!name) return
 
       this.todos.push({
-        id: this.index++, name
+        id: this.index++,
+        name,
+        isDone: false
+      })
+    },
+    onDone: function (id) {
+      for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].id === id) {
+          this.todos[i].isDone = true
+        }
+      }
+    }
+  },
+  computed: {
+    itemsToDo () {
+      return this.todos.filter(item => {
+        return item.isDone === false
       })
     }
   }
@@ -45,4 +64,7 @@ export default {
 
 
 <style scoped>
+.items {
+  margin: 50px 0 0;
+}
 </style>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,13 +1,38 @@
 <template>
-  <p>{{item.id}}: {{item.name}}</p>
+  <div class="container">
+    <div class="name">{{item.name}}</div>
+    <div 
+      class="btn btn-remove"
+      v-on:click="done()">
+      Done
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'todo-item',
-  props: ['item']
+  props: ['item', 'onDone'],
+  methods: {
+    done () {
+      this.onDone(this.item.id)
+    }
+  }
 }
 </script>
 
 <style scoped>
+.container {
+  margin: 8px 0;
+  display: flex;
+}
+.name {
+  flex: 5;
+  text-align: left;
+}
+.btn {
+  flex: 1;
+}
+.btn-remove {
+}
 </style>
